3a7393aba70f232582b924a15f5027253877d111
leonardoandrade1 <leonardoandrade0@outlook.com>
2025-12-31 15:31:18 -0300

    refactor: make nginx config simpler

--- aws-ec2/userdata.sh
+++ aws-ec2/userdata.sh
@@ -64,31 +64,25 @@ sudo -u deploy-user PM2_HOME=/opt/.pm2 pm2 save
 ## Configure PM2 to start on boot
 sudo su -c "PM2_HOME=/opt/.pm2 pm2 startup systemd -u deploy-user --hp /home/deploy-user"
 
-## Remove all default NGINX files
-# rm -f /etc/nginx/nginx.conf
+## Remove default NGINX config to avoid conflicts
 rm -f /etc/nginx/conf.d/*.conf
-# rm -f /etc/nginx/default.d/
 
-## Configure a simlink for NGINX conf
-ln -sf /var/www/app/nginx/nginx.conf /etc/nginx/nginx.conf
+## Copy custom nginx server config from the repository
+cp /var/www/app/nginx/nginx.conf /etc/nginx/conf.d/node_app.conf
 
 ## Set proper permissions for NGINX logs
 touch /var/log/nginx/node_app_error.log /var/log/nginx/node_app_access.log
 chown nginx:nginx /var/log/nginx/node_app_*.log
-chown -R root:nginx /var/www/app
-chmod -R 755 /var/www/app
+
 ## Allow NGINX make network connections for SELinux
 setsebool -P httpd_can_network_connect 1
 
-## Configure NGINX
+## Apply custom NGINX config
 if [ -f "/var/www/app/nginx/nginx.conf" ]; then
     ## Remove the default config to avoid conflicts
     rm -f /etc/nginx/conf.d/*.conf
     # Copy custom NGINX config from the repository
     cp /var/www/app/nginx/nginx.conf /etc/nginx/conf.d/node_app.conf
-
-    touch /var/log/nginx/node_app_error.log
-    chown nginx:nginx /var/log/nginx/node_app_error.log
 fi
 
 ## Restart NGINX to apply changes
